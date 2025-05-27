1f34818fce99ded7dd625b4ffd6eee1b426e6e15
MateusBMP <bmp.mateus@gmail.com>
2025-05-27 19:46:52 -0300

    nome do apoio da reclamaçao alterado

--- resources/views/livewire/up-incident-zone.blade.php
+++ resources/views/livewire/up-incident-zone.blade.php
@@ -3,7 +3,7 @@
         <h1 class="text-5xl w-24">{{ $this->up_count }}</h1>
         @if (!$this->upping_incident)
             <button type="button" class="inline-flex items-center px-6 py-3 border border-transparent shadow-sm text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 cursor-pointer" wire:click="upIncident">
-                Promover ocorrência
+                Apoiar reclamação
                 <!-- Heroicon name: solid/chevron-right -->
                 <svg class="-mr-1 ml-3 h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
                     <path fill-rule="evenodd" d="M10.293 15.293a1 1 0 001.414 0l4-4a1 1 0 000-1.414l-4-4a1 1 0 00-1.414 1.414L13.586 10H3a1 1 0 100 2h10.586l-3.293 3.293a1 1 0 000 1.414z" clip-rule="evenodd" />
@@ -11,7 +11,7 @@
             </button>
         @else
             <button type="button" class="inline-flex items-center px-6 py-3 border border-transparent shadow-sm text-base font-medium rounded-md text-white bg-green-600 hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500 cursor-not-allowed">
-                Ocorrência promovida
+                Reclamação apoiada
                 <!-- Heroicon name: solid/chevron-right -->
                 <svg class="-mr-1 ml-3 h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
                     <path fill-rule="evenodd" d="M10.293 15.293a1 1 0 001.414 0l4-4a1 1 0 000-1.414l-4-4a1 1 0 00-1.414 1.414L13.586 10H3a1 1 0 100 2h10.586l-3.293 3.293a1 1 0 000 1.414z" clip-rule="evenodd" />
